a0a5a2cbbb620d010279177880c95b909e1309c2
ItzInten <bpridatko@gmail.com>
2025-04-26 14:24:50 +0100

    weather page slightly fixed

--- FinalProject/WeatherPage.xaml.cs
+++ FinalProject/WeatherPage.xaml.cs
@@ -72,7 +72,6 @@ namespace FinalProject
             await GetLocation();
             var result = await WeatherAPIService.GetWeather(latitude, longitude);
             string getIconUrl = getWeatherIcon(result.current.weather_code, result.current.is_day);
-            WeatherIcon.Source = getIconUrl;
             var cityName = await WeatherAPIService.GetCityDetails(Math.Round(latitude, 4), Math.Round(longitude, 4));
             CityNameLabel.Text = cityName.features[0].properties.city;
             TemperatureLabel.Text = result.current.temperature_2m.ToString() + result.current_units.temperature_2m;
@@ -85,6 +84,8 @@ namespace FinalProject
                 if (formattedDate.ToString() == result.hourly.time[i])
                 {
                     currentIndex = i;
+                    getIconUrl = getWeatherIcon(result.hourly.weather_code[i], result.hourly.is_day[i]);
+                    WeatherIcon.Source = getIconUrl;
                 }
                 WeatherDataCombined.Add(new HourlyWeatherData
                 {
